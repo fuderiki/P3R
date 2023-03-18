f89af7fffc1470064cc87dcdf43ded5b686f4c91
fuderiki <frederic.seraphine@gmail.com>
2023-03-18 17:16:24 +0900

    Update transaction_tests.rs

--- src/tests/transaction_tests.rs
+++ src/tests/transaction_tests.rs
@@ -1,1 +1,16 @@
+use crate::modules::transaction::Transaction;
+
+#[test]
+fn test_transaction_creation() {
+    let from = String::from("sender");
+    let to = String::from("recipient");
+    let amount = 10.0;
+
+    let transaction = Transaction::new(from.clone(), to.clone(), amount);
+
+    assert_eq!(transaction.from, from);
+    assert_eq!(transaction.to, to);
+    assert_eq!(transaction.amount, amount);
+    // Add assertions to test the timestamp and other fields of the Transaction struct
+}
 
