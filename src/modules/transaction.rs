b851bbc5ce5b8859af2fe4d57d2293c454d61a7a
fuderiki <frederic.seraphine@gmail.com>
2023-03-18 17:14:38 +0900

    Update transaction.rs

--- src/modules/transaction.rs
+++ src/modules/transaction.rs
@@ -1,1 +1,25 @@
+pub struct Transaction {
+    pub from: String,
+    pub to: String,
+    pub amount: f32,
+    pub timestamp: u64,
+}
+
+impl Transaction {
+    pub fn new(from: String, to: String, amount: f32) -> Self {
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs();
+
+        Self {
+            from,
+            to,
+            amount,
+            timestamp,
+        }
+    }
+
+    // Add other methods for your Transaction operations
+}
 
