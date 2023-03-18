9c0264e82b6d049503cc9d99ce73e63c3f4d6956
fuderiki <frederic.seraphine@gmail.com>
2023-03-18 16:56:16 +0900

    Update dag_tests.rs

--- src/tests/dag_tests.rs
+++ src/tests/dag_tests.rs
@@ -1,1 +1,11 @@
+use crate::modules::dag::DAG;
+
+#[test]
+fn test_dag_creation() {
+    let dag = DAG::new();
+
+    // Add assertions to test the creation of your DAG
+}
+
+// Add more test functions for other DAG methods and operations
 
