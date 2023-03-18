4ed9989e4424be451dea324c14a7db87813741a3
fuderiki <frederic.seraphine@gmail.com>
2023-03-18 17:09:19 +0900

    Update block_tests.rs

--- src/tests/block_tests.rs
+++ src/tests/block_tests.rs
@@ -1,1 +1,14 @@
+use crate::modules::block::Block;
+
+#[test]
+fn test_block_creation() {
+    let prev_hash = String::from("previous block hash");
+    let data = String::from("data");
+
+    let block = Block::new(prev_hash, data);
+
+    assert_eq!(block.prev_hash, "previous block hash");
+    assert_eq!(block.data, "data");
+    // Add assertions to test other fields of the Block struct
+}
 
