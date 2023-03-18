155c448c951df588b40d08462dca17da9735d699
fuderiki <frederic.seraphine@gmail.com>
2023-03-18 16:55:20 +0900

    Update dag.rs

--- src/modules/dag.rs
+++ src/modules/dag.rs
@@ -1,1 +1,14 @@
+pub struct DAG {
+    // Your DAG fields go here
+}
+
+impl DAG {
+    pub fn new() -> Self {
+        // Initialize your DAG and return it
+    }
+
+    // Add other methods for your DAG operations
+}
+
+// Add additional structs, enums, and implementations as needed
 
