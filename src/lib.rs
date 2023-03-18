d201671e59b2d0696b6cd02073768b16de78c70c
fuderiki <frederic.seraphine@gmail.com>
2023-03-18 18:39:13 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-pub mod dag;
-pub mod transaction;
-pub mod consensus;
-pub mod block;
+pub mod modules {
+    pub mod dag;
+    pub mod transaction;
+    pub mod consensus;
+    pub mod block;
+}
 
 
 // Export any types or functions needed by the other modules here
