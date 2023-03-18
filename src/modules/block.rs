be48e05cd6cd79c664093fc3be48e70cee37fb3e
fuderiki <frederic.seraphine@gmail.com>
2023-03-18 17:06:43 +0900

    Update block.rs

--- src/modules/block.rs
+++ src/modules/block.rs
@@ -1,1 +1,29 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub struct Block {
+    pub prev_hash: String,
+    pub timestamp: u64,
+    pub data: String,
+    pub hash: String,
+}
+
+impl Block {
+    pub fn new(prev_hash: String, data: String) -> Self {
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs();
+
+        let hash = String::new(); // Replace with hash function
+
+        Self {
+            prev_hash,
+            timestamp,
+            data,
+            hash,
+        }
+    }
+
+    // Add other methods for your Block operations
+}
 
