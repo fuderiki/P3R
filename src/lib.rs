77af29eda73485db62f9fbb36cc39502183ce2e0
fuderiki <frederic.seraphine@gmail.com>
2023-03-18 17:03:25 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
+pub mod dag {
+    // Your DAG implementation goes here
+}
 
+pub mod block {
+    // Your Block implementation goes here
+}
+
+pub mod transaction {
+    // Your Transaction implementation goes here
+}
+
+// Export any types or functions needed by the other modules here
